646dda2b8102b0111e7d6612771426898a216bb8
PreviousHan4663 <obxora@proton.me>
2026-05-06 01:53:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from datetime import datetime
 from flask import Flask, redirect, request
 app = Flask(__name__)
 
+TARGET = "https://cloud.mail.ru/public/C3Nj/"
+
 @app.route("/")
 def go():
     # тут ты ловишь факт перехода
@@ -12,7 +14,10 @@ def go():
     print("IP:", user_ip)
     print("UA:", user_agent)
 
-    return redirect("https://cloud.mail.ru/public/C3Nj/8JYTes8G9")
+    # редирект на нужную страницу
+    with open("clicks.log", "a", encoding="utf-8") as f:
+        f.write(f"Время - {datetime.now()} | IP - [{user_ip} | Еще что-то {user_agent}\n")
+    return redirect("https://cloud.mail.ru/public/dmhY/2rAoRormJ")
 
 if __name__ == "__main__":
     app.run(debug=True)
